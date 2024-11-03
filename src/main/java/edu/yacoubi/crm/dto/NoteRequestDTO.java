a607c96c18b503ad6b0244876b09981953dd7fa8
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-11-03 22:51:53 +0100

    NoteRequestDTO validation specification & schema description for Swagger UI

--- src/main/java/edu/yacoubi/crm/dto/NoteRequestDTO.java
+++ src/main/java/edu/yacoubi/crm/dto/NoteRequestDTO.java
@@ -1,6 +1,10 @@
 package edu.yacoubi.crm.dto;
 
 import edu.yacoubi.crm.model.InteractionType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,8 +13,17 @@ import java.time.LocalDate;
 @Data
 @Builder
 public class NoteRequestDTO {
+    @NotBlank(message = "Content is mandatory")
+    @Size(max = 1000, message = "Content must be less than 1000 characters")
     private String content;
+
+    @NotNull(message = "Date is mandatory")
     private LocalDate date;
+
+    @Schema(description = "The type of interaction", example = "PHONE_CALL")
+    @NotNull(message = "Interaction type is mandatory")
     private InteractionType interactionType;
+
+    @Schema(description = "The ID of the customer. This field is optional as it can also be provided as a path variable.")
     private Long customerId;
 }
