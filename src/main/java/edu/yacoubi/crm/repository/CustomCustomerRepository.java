e7bc49af257b2876df9cdc36624eed0e5dd3b640
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-12-23 17:12:27 +0100

    restore

--- src/main/java/edu/yacoubi/crm/repository/CustomCustomerRepository.java
+++ src/main/java/edu/yacoubi/crm/repository/CustomCustomerRepository.java
@@ -2,8 +2,6 @@ package edu.yacoubi.crm.repository;
 
 import edu.yacoubi.crm.dto.customer.CustomerRequestDTO;
 import edu.yacoubi.crm.model.Customer;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface CustomCustomerRepository {
     Customer  updateCustomerByExample(CustomerRequestDTO customerExample, Long id);
