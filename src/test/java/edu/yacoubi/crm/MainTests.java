50f71790ec8c31a7a94f240376ad073dd495c202
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-12-30 23:44:54 +0100

    clean up

--- src/test/java/edu/yacoubi/crm/MainTests.java
+++ src/test/java/edu/yacoubi/crm/MainTests.java
@@ -3,10 +3,10 @@ package edu.yacoubi.crm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MainTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
