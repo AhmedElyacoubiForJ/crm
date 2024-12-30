abf507b8fa8181bc3ffae8e325704a20b4e181a5
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-12-30 22:53:09 +0100

    Reafactor: test using TestDataUtil

--- src/test/java/edu/yacoubi/crm/util/EntityTransformerTest.java
+++ src/test/java/edu/yacoubi/crm/util/EntityTransformerTest.java
@@ -1,5 +1,6 @@
 package edu.yacoubi.crm.util;
 
+import edu.yacoubi.crm.TestDataUtil;
 import edu.yacoubi.crm.dto.customer.CustomerRequestDTO;
 import edu.yacoubi.crm.dto.customer.CustomerResponseDTO;
 import edu.yacoubi.crm.dto.employee.EmployeeRequestDTO;
@@ -7,7 +8,6 @@ import edu.yacoubi.crm.dto.employee.EmployeeResponseDTO;
 import edu.yacoubi.crm.dto.note.NoteResponseDTO;
 import edu.yacoubi.crm.model.Customer;
 import edu.yacoubi.crm.model.Employee;
-import edu.yacoubi.crm.model.InteractionType;
 import edu.yacoubi.crm.model.Note;
 import org.junit.jupiter.api.Test;
 
@@ -22,154 +22,132 @@ public class EntityTransformerTest {
     @Test
     public void testNoteToNoteResponseDTOTransformation() {
         // Given
-        Note note = Note.builder()
-                .id(1L)
-                .content("Test Note")
-                .date(LocalDate.now())
-                .interactionType(InteractionType.EMAIL)
-                .customer(Customer.builder().id(2L).build())
-                .build();
+        Customer customerA = TestDataUtil.createCustomerA(null);
+        customerA.setId(2L);
+        Note noteA = TestDataUtil.createNoteA(customerA);
+        noteA.setId(1L);
 
         // When
-        NoteResponseDTO noteDTO = noteToNoteResponseDto.transform(note);
-        NoteResponseDTO noteDTOViaUtil = TransformerUtil.transform(noteToNoteResponseDto, note);
+        NoteResponseDTO noteADTO = noteToNoteResponseDto.transform(noteA);
+        NoteResponseDTO noteDTOViaUtil = TransformerUtil.transform(noteToNoteResponseDto, noteA);
 
         // Then
-        assertEquals(1L, noteDTO.getId());
-        assertEquals("Test Note", noteDTO.getContent());
-        assertEquals(InteractionType.EMAIL, noteDTO.getInteractionType());
-        assertEquals(2L, noteDTO.getCustomerId());
+        assertEquals(noteA.getId(), noteADTO.getId());
+        assertEquals(noteA.getContent(), noteADTO.getContent());
+        assertEquals(noteA.getInteractionType(), noteADTO.getInteractionType());
+        assertEquals(noteA.getCustomer().getId(), noteADTO.getCustomerId());
 
-        assertEquals(1L, noteDTOViaUtil.getId());
-        assertEquals("Test Note", noteDTOViaUtil.getContent());
-        assertEquals(InteractionType.EMAIL, noteDTOViaUtil.getInteractionType());
-        assertEquals(2L, noteDTOViaUtil.getCustomerId());
+        assertEquals(noteA.getId(), noteDTOViaUtil.getId());
+        assertEquals(noteA.getContent(), noteDTOViaUtil.getContent());
+        assertEquals(noteA.getInteractionType(), noteDTOViaUtil.getInteractionType());
+        assertEquals(noteA.getCustomer().getId(), noteDTOViaUtil.getCustomerId());
     }
 
     @Test
     public void testEmployeeToEmployeeResponseDTOTransformation() {
         // Given
-        Employee employee = Employee.builder()
-                .id(1L)
-                .firstName("John")
-                .lastName("Doe")
-                .email("john.doe@example.com")
-                .department("Sales")
-                .build();
+        Employee employeeB = TestDataUtil.createEmployeeB();
+        employeeB.setId(1L);
 
         // When
-        EmployeeResponseDTO employeeDTO = employeeToEmployeeResponseDto.transform(employee);
+        EmployeeResponseDTO employeeDTO =
+                employeeToEmployeeResponseDto.transform(employeeB);
         EmployeeResponseDTO employeeDTOViaUtil =
-                TransformerUtil.transform(employeeToEmployeeResponseDto, employee);
+                TransformerUtil.transform(employeeToEmployeeResponseDto, employeeB);
 
         // Then
-        assertEquals(1L, employeeDTO.getId());
-        assertEquals("John", employeeDTO.getFirstName());
-        assertEquals("Doe", employeeDTO.getLastName());
-        assertEquals("john.doe@example.com", employeeDTO.getEmail());
-        assertEquals("Sales", employeeDTO.getDepartment());
+        assertEquals(employeeDTO.getId(), employeeDTO.getId());
+        assertEquals(employeeDTO.getFirstName(), employeeDTO.getFirstName());
+        assertEquals(employeeDTO.getLastName(), employeeDTO.getLastName());
+        assertEquals(employeeDTO.getEmail(), employeeDTO.getEmail());
+        assertEquals(employeeDTO.getDepartment(), employeeDTO.getDepartment());
 
-        assertEquals(1L, employeeDTOViaUtil.getId());
-        assertEquals("John", employeeDTOViaUtil.getFirstName());
-        assertEquals("Doe", employeeDTOViaUtil.getLastName());
-        assertEquals("john.doe@example.com", employeeDTOViaUtil.getEmail());
-        assertEquals("Sales", employeeDTOViaUtil.getDepartment());
+        assertEquals(employeeDTO.getId(), employeeDTOViaUtil.getId());
+        assertEquals(employeeDTO.getFirstName(), employeeDTOViaUtil.getFirstName());
+        assertEquals(employeeDTO.getLastName(), employeeDTOViaUtil.getLastName());
+        assertEquals(employeeDTO.getEmail(), employeeDTOViaUtil.getEmail());
+        assertEquals(employeeDTO.getDepartment(), employeeDTOViaUtil.getDepartment());
     }
 
     @Test
     public void testEmployeeRequestDTOToEmployeeTransformation() {
         // Given
-        EmployeeRequestDTO employeeRequestDTO = EmployeeRequestDTO.builder()
-                .firstName("Jane")
-                .lastName("Smith")
-                .email("jane.smith@example.com")
-                .department("Marketing")
-                .build();
+        EmployeeRequestDTO employeeRequestDTOA = TestDataUtil.createEmployeeRequestDTOA();
 
         // When
-        Employee employee = employeeRequestDtoToEmployee.transform(employeeRequestDTO);
-        Employee employeeViaUtil = TransformerUtil.transform(employeeRequestDtoToEmployee, employeeRequestDTO);
+        Employee employee = employeeRequestDtoToEmployee.transform(employeeRequestDTOA);
+        Employee employeeViaUtil = TransformerUtil
+                .transform(employeeRequestDtoToEmployee, employeeRequestDTOA);
 
         // Then
-        assertEquals("Jane", employee.getFirstName());
-        assertEquals("Smith", employee.getLastName());
-        assertEquals("jane.smith@example.com", employee.getEmail());
-        assertEquals("Marketing", employee.getDepartment());
+        assertEquals(employeeRequestDTOA.getFirstName(), employee.getFirstName());
+        assertEquals(employeeRequestDTOA.getLastName(), employee.getLastName());
+        assertEquals(employeeRequestDTOA.getEmail(), employee.getEmail());
+        assertEquals(employeeRequestDTOA.getDepartment(), employee.getDepartment());
 
-        assertEquals("Jane", employeeViaUtil.getFirstName());
-        assertEquals("Smith", employeeViaUtil.getLastName());
-        assertEquals("jane.smith@example.com", employeeViaUtil.getEmail());
-        assertEquals("Marketing", employeeViaUtil.getDepartment());
+        assertEquals(employeeRequestDTOA.getFirstName(), employeeViaUtil.getFirstName());
+        assertEquals(employeeRequestDTOA.getLastName(), employeeViaUtil.getLastName());
+        assertEquals(employeeRequestDTOA.getEmail(), employeeViaUtil.getEmail());
+        assertEquals(employeeRequestDTOA.getDepartment(), employeeViaUtil.getDepartment());
     }
 
     @Test
     public void testCustomerRequestDTOToCustomerTransformation() {
         // Given
-        CustomerRequestDTO customerRequestDTO = CustomerRequestDTO.builder()
-                .firstName("Jane")
-                .lastName("Smith")
-                .email("jane.smith@example.com")
-                .phone("123456789")
-                .address("123 Main St")
-                .lastInteractionDate(LocalDate.now())
-                .build();
+        CustomerRequestDTO customerRequestDTOA = TestDataUtil.createCustomerRequestDTOA();
 
         // When
-        Customer customer = customerRequestDtoToCustomer.transform(customerRequestDTO);
-        Customer customerViaUtil = TransformerUtil.transform(customerRequestDtoToCustomer, customerRequestDTO);
+        Customer customer = customerRequestDtoToCustomer.transform(customerRequestDTOA);
+        Customer customerViaUtil = TransformerUtil
+                .transform(customerRequestDtoToCustomer, customerRequestDTOA);
 
         // Then
-        assertEquals("Jane", customer.getFirstName());
-        assertEquals("Smith", customer.getLastName());
-        assertEquals("jane.smith@example.com", customer.getEmail());
-        assertEquals("123456789", customer.getPhone());
-        assertEquals("123 Main St", customer.getAddress());
-        assertEquals(customerRequestDTO.getLastInteractionDate(), customer.getLastInteractionDate());
+        assertEquals(customerRequestDTOA.getFirstName(), customer.getFirstName());
+        assertEquals(customerRequestDTOA.getLastName(), customer.getLastName());
+        assertEquals(customerRequestDTOA.getEmail(), customer.getEmail());
+        assertEquals(customerRequestDTOA.getPhone(), customer.getPhone());
+        assertEquals(customerRequestDTOA.getAddress(), customer.getAddress());
+        assertEquals(customerRequestDTOA.getLastInteractionDate(), customer.getLastInteractionDate());
 
-        assertEquals("Jane", customerViaUtil.getFirstName());
-        assertEquals("Smith", customerViaUtil.getLastName());
-        assertEquals("jane.smith@example.com", customerViaUtil.getEmail());
-        assertEquals("123456789", customerViaUtil.getPhone());
-        assertEquals("123 Main St", customerViaUtil.getAddress());
-        assertEquals(customerRequestDTO.getLastInteractionDate(), customerViaUtil.getLastInteractionDate());
+        assertEquals(customerRequestDTOA.getFirstName(), customerViaUtil.getFirstName());
+        assertEquals(customerRequestDTOA.getLastName(), customerViaUtil.getLastName());
+        assertEquals(customerRequestDTOA.getEmail(), customerViaUtil.getEmail());
+        assertEquals(customerRequestDTOA.getPhone(), customerViaUtil.getPhone());
+        assertEquals(customerRequestDTOA.getAddress(), customerViaUtil.getAddress());
+        assertEquals(customerRequestDTOA.getLastInteractionDate(), customerViaUtil.getLastInteractionDate());
     }
 
     @Test
     public void testCustomerToCustomerResponseDTOTransformation() {
         // Given
-        Customer customer = Customer.builder()
-                .id(1L)
-                .firstName("John")
-                .lastName("Doe")
-                .email("john.doe@example.com")
-                .phone("987654321")
-                .address("456 Elm St")
-                .lastInteractionDate(LocalDate.now())
-                .employee(Employee.builder().id(3L).build())
-                .build();
+        Employee employeeB = TestDataUtil.createEmployeeB();
+        employeeB.setId(3L);
+        Customer customerA = TestDataUtil.createCustomerA(employeeB);
+        customerA.setId(1L);
 
         // When
-        CustomerResponseDTO customerDTO = customerToCustomerResponseDto.transform(customer);
-        CustomerResponseDTO customerDTOViaUtil = TransformerUtil.transform(customerToCustomerResponseDto, customer);
+        CustomerResponseDTO customerDTO = customerToCustomerResponseDto.transform(customerA);
+        CustomerResponseDTO customerDTOViaUtil = TransformerUtil
+                .transform(customerToCustomerResponseDto, customerA);
 
         // Then
-        assertEquals(1L, customerDTO.getId());
-        assertEquals("John", customerDTO.getFirstName());
-        assertEquals("Doe", customerDTO.getLastName());
-        assertEquals("john.doe@example.com", customerDTO.getEmail());
-        assertEquals("987654321", customerDTO.getPhone());
-        assertEquals("456 Elm St", customerDTO.getAddress());
-        assertEquals(customer.getLastInteractionDate(), customerDTO.getLastInteractionDate());
-        assertEquals(3L, customerDTO.getEmployeeId());
+        assertEquals(customerA.getId(), customerDTO.getId());
+        assertEquals(customerA.getFirstName(), customerDTO.getFirstName());
+        assertEquals(customerA.getLastName(), customerDTO.getLastName());
+        assertEquals(customerA.getEmail(), customerDTO.getEmail());
+        assertEquals(customerA.getPhone(), customerDTO.getPhone());
+        assertEquals(customerA.getAddress(), customerDTO.getAddress());
+        assertEquals(customerA.getLastInteractionDate(), customerDTO.getLastInteractionDate());
+        assertEquals(customerA.getEmployee().getId(), customerDTO.getEmployeeId());
 
-        assertEquals(1L, customerDTOViaUtil.getId());
-        assertEquals("John", customerDTOViaUtil.getFirstName());
-        assertEquals("Doe", customerDTOViaUtil.getLastName());
-        assertEquals("john.doe@example.com", customerDTOViaUtil.getEmail());
-        assertEquals("987654321", customerDTOViaUtil.getPhone());
-        assertEquals("456 Elm St", customerDTOViaUtil.getAddress());
-        assertEquals(customer.getLastInteractionDate(), customerDTOViaUtil.getLastInteractionDate());
-        assertEquals(3L, customerDTOViaUtil.getEmployeeId());
+        assertEquals(customerA.getId(), customerDTOViaUtil.getId());
+        assertEquals(customerA.getFirstName(), customerDTOViaUtil.getFirstName());
+        assertEquals(customerA.getLastName(), customerDTOViaUtil.getLastName());
+        assertEquals(customerA.getEmail(), customerDTOViaUtil.getEmail());
+        assertEquals(customerA.getPhone(), customerDTOViaUtil.getPhone());
+        assertEquals(customerA.getAddress(), customerDTOViaUtil.getAddress());
+        assertEquals(customerA.getLastInteractionDate(), customerDTOViaUtil.getLastInteractionDate());
+        assertEquals(customerA.getEmployee().getId(), customerDTOViaUtil.getEmployeeId());
     }
 
     @Test
