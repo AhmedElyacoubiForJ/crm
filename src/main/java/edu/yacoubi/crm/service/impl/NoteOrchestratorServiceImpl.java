b5210784a08cefdc91bafb27a49a037ae6b8d48a
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2025-01-18 17:03:36 +0100

    docs: Add JavaDocs and logging statements to NoteOrchestratorServiceImpl class and methods refactor: Mark method parameters as final and improve code readability

--- src/main/java/edu/yacoubi/crm/service/impl/NoteOrchestratorServiceImpl.java
+++ src/main/java/edu/yacoubi/crm/service/impl/NoteOrchestratorServiceImpl.java
@@ -9,6 +9,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+/**
+ * Implementation of the Note Orchestrator Service.
+ *
+ * <p>This class implements the logic for orchestrating note creation for customers, including
+ * validating the customer and delegating the note creation to the note service.</p>
+ *
+ * @author Your Name
+ */
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -16,19 +24,29 @@ public class NoteOrchestratorServiceImpl implements INoteOrchestratorService {
     private final ICustomerService customerService;
     private final INoteService noteService;
 
+    /**
+     * Creates a note for a customer.
+     *
+     * @param note       the note to be created
+     * @param customerId the ID of the customer for whom the note is being created
+     * @return the created note
+     */
     @Override
-    public Note createNoteForCustomer(Note note, Long customerId) {
-        log.info("::createNoteForCustomer started with: note {}, customerId: {}", note, customerId);
+    public Note createNoteForCustomer(final Note note, final Long customerId) {
+        if (log.isInfoEnabled()) {
+            log.info("::createNoteForCustomer started with: note {}, customerId: {}", note, customerId);
+        }
 
         // Abruf des Kunden mit get(), da die Validierung im CustomerService stattfindet
-        Customer customer = customerService.getCustomerById(customerId).get();
+        final Customer customer = customerService.getCustomerById(customerId).get();
 
         // Setzen des Kunden und Delegation an NoteService
         note.setCustomer(customer);
-        Note savedNote = noteService.createNote(note);
+        final Note savedNote = noteService.createNote(note);
 
-        log.info("::createNoteForCustomer completed successfully with: noteId {}", savedNote.getId());
+        if (log.isInfoEnabled()) {
+            log.info("::createNoteForCustomer completed successfully with: noteId {}", savedNote.getId());
+        }
         return savedNote;
     }
 }
-
